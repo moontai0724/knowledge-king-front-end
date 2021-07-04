9e424dec9d501229b324d260253e968ccc6645f0
月太げつたい <me@moontai0724.tw>
2021-07-05 01:22:47 +0800

    fix: font not displaying

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['@mdi/font/css/materialdesignicons.min.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],

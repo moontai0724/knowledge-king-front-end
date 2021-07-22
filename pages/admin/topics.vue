9e1b21d7511e0da2ffebc038ce47290f167adf0a
月太げつたい <me@moontai0724.tw>
2021-07-22 13:36:07 +0800

    feat: create topic button

--- pages/admin/topics.vue
+++ pages/admin/topics.vue
@@ -17,6 +17,32 @@
           single-line
           hide-details
         ></v-text-field>
+        <v-dialog v-model="dialog">
+          <template #activator="{ on, attrs }">
+            <v-btn
+              disabled
+              color="green"
+              class="ml-2 white--text"
+              v-bind="attrs"
+              v-on="on"
+            >
+              新開主題
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">新開主題</span>
+            </v-card-title>
+
+            <v-card-text></v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="close"> Cancel </v-btn>
+              <v-btn color="blue darken-1" text @click="save"> Save </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </v-toolbar>
     </template>
     <template #[`item.actions`]="{ item }">
